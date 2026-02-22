f3378dd7ba5902c202b4e137171eb443607114f0
adriangt2001 <adriangt2001@hotmail.com>
2026-02-22 22:06:04 +0000

    Added alpha and rho search functionality for task 2

--- Week1/main.py
+++ Week1/main.py
@@ -2,37 +2,54 @@ import argparse
 from task1.task1 import run_task1
 from task2.task2 import run_task2
 from utils import create_detection_gif
+import yaml
 
 def main():
 
     parser = argparse.ArgumentParser(description="MCV Week1 Project")
 
-    parser.add_argument("--task", type=str, required=True)
+    parser.add_argument("--task", type=str, default=None)
 
-    parser.add_argument("--video", type=str, required=True,
+    parser.add_argument("--video", type=str, default=None,
                         help="Path to video")
 
-    parser.add_argument("--annotations", type=str, required=True,
+    parser.add_argument("--annotations", type=str, default=None,
                         help="Path to XML annotations")
     
+    parser.add_argument("--config", type=str, default=None)
     parser.add_argument("--dataset", type=str, default='dataset')
     parser.add_argument("--train_ratio", type=float, default=0.25)
 
-    parser.add_argument("--alpha", type=float, default=3.0)
+    parser.add_argument("--alpha", nargs='+', type=float, default=[3.0])
     parser.add_argument("--min_area", type=int, default=1500)
     parser.add_argument("--open_size", type=int, default=3)
     parser.add_argument("--close_size", type=int, default=7)
-    parser.add_argument("--rho", type=float, default=0.01)
+    parser.add_argument("--rho", nargs='+', type=float, default=[0.01])
 
     parser.add_argument("--gif", action="store_true", help="Generate detection GIF at the end")
 
     args = parser.parse_args()
 
+    if args.config:
+        with open(args.config, "r") as f:
+            yaml_config = yaml.safe_load(f)
+            
+        # 4. Overwrite the parser defaults with YAML values
+        # This keeps the command line flags as the highest priority
+        parser.set_defaults(**yaml_config)
+        
+        # 5. Re-parse to finalize the values
+        args = parser.parse_args()
+    
+    assert args.task
+    assert args.video
+    assert args.annotations
+
     if args.task == "task1":
         test_frames, all_boxes, gt_dict, train_end = run_task1(args)
 
     elif args.task == "task2":
-        test_frames, all_boxes, gt_dict, train_end = run_task2(args)
+        test_frames, all_boxes, gt_dict, train_end, alpha, rho = run_task2(args)
 
     else:
         raise ValueError("Unknown task")
@@ -43,7 +60,7 @@ def main():
             all_pred_boxes=all_boxes, 
             gt_per_frame=gt_dict, 
             train_end=train_end,
-            output_path=f"{args.task}/outputs/detections_alpha_{args.alpha}.gif",
+            output_path=f"{args.task}/outputs/detections_alpha_{alpha}_{rho}.gif",
             max_frames=100
         )
 
--- Week1/task2/task2.py
+++ Week1/task2/task2.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
 from task1.task1 import load_video_frames, load_ground_truth, gt_to_coco, preds_to_coco, evaluate_coco, get_bounding_boxes, remove_nested_boxes, merge_overlapping_boxes
+from itertools import product
 
-def segment_and_detect(test_frames, mu, sigma, roi, rho, args):
+def segment_and_detect(test_frames, mu, sigma, roi, alpha, rho, args):
     all_pred_boxes = []
     
     current_mu = mu.copy()
@@ -17,7 +18,7 @@ def segment_and_detect(test_frames, mu, sigma, roi, rho, args):
         diff = np.abs(frame - current_mu)
         sigma_safe = np.maximum(current_sigma, 1e-6)
 
-        mask_bool = (diff >= args.alpha * (sigma_safe + 2)) & roi
+        mask_bool = (diff >= alpha * (sigma_safe + 2)) & roi
         mask_uint8 = mask_bool.astype(np.uint8) * 255
 
         mask_proc = cv2.morphologyEx(mask_uint8, cv2.MORPH_OPEN, kernel_open)
@@ -43,6 +44,7 @@ def segment_and_detect(test_frames, mu, sigma, roi, rho, args):
 
     return all_pred_boxes
 
+
 def run_task2(args):
     """
     Task 2: Adaptive Background Estimation using a Recursive Gaussian Model.
@@ -50,29 +52,40 @@ def run_task2(args):
     classified as background to adapt to lighting changes.
     """
     print(f"Running Task 2: Adaptive Gaussian (alpha={args.alpha}, rho={args.rho}) ---")
+    best_alpha = None
+    best_rho = None
+    best_ap50 = -1
 
-    frames = load_video_frames(args.video)
-    N = len(frames)
-    train_end = int(0.25 * N)
-    train = frames[:train_end]
-    test = frames[train_end:]
-    mu = np.mean(train, axis=0)
-    sigma = np.std(train, axis=0)
-    roi = cv2.imread(
-        args.video.replace("vdo.avi", "roi.jpg"),
-        cv2.IMREAD_GRAYSCALE
-    )
-    roi = roi > 0
+    for alpha, rho in product(args.alpha, args.rho):
+        frames = load_video_frames(args.video)
+        N = len(frames)
+        train_end = int(0.25 * N)
+        train = frames[:train_end]
+        test = frames[train_end:]
+        mu = np.mean(train, axis=0)
+        sigma = np.std(train, axis=0)
+        roi = cv2.imread(
+            args.video.replace("vdo.avi", "roi.jpg"),
+            cv2.IMREAD_GRAYSCALE
+        )
+        roi = roi > 0
 
-    all_pred_boxes = segment_and_detect(test, mu, sigma, roi, args.rho, args)
+        all_pred_boxes = segment_and_detect(test, mu, sigma, roi, alpha, rho, args)
 
-    gt_dict = load_ground_truth(args.annotations)
-    
-    gt_json = gt_to_coco(gt_dict, train_end, len(test))
-    pred_json = preds_to_coco(all_pred_boxes, train_end)
+        gt_dict = load_ground_truth(args.annotations)
+        
+        gt_json = gt_to_coco(gt_dict, train_end, len(test))
+        pred_json = preds_to_coco(all_pred_boxes, train_end)
 
-    ap50 = evaluate_coco(gt_json, pred_json)
+        ap50 = evaluate_coco(gt_json, pred_json)
 
-    print(f"\nFinal Adaptive AP50: {ap50:.4f}")
+        if ap50 > best_ap50:
+            best_ap50 = ap50
+            best_alpha = alpha
+            best_rho = rho
 
-    return test, all_pred_boxes, gt_dict, train_end
+    print(f"\nFinal Adaptive AP50: {best_ap50:.4f}")
+    print(f"\nFinal Alpha: {best_alpha:.4f}")
+    print(f"\nFinal Rho: {best_rho:.4f}")
+
+    return test, all_pred_boxes, gt_dict, train_end, best_alpha, best_rho
