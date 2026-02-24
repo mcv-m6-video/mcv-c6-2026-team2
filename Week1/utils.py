484986559b86debd44cd09a6eb8d82e92e0259a6
adriangt2001 <adriangt2001@hotmail.com>
2026-02-24 13:28:39 +0000

    Gif from video

--- Week1/utils.py
+++ Week1/utils.py
@@ -3,6 +3,7 @@ import os
 import cv2
 import numpy as np
 from tqdm import tqdm
+from pygifsicle import optimize
 
 def create_detection_video(video_path,
                          all_pred_boxes,
@@ -91,3 +92,86 @@ def create_detection_gif(test_frames,
     imageio.mimsave(output_path, frames_for_gif, fps=10, loop=0)
 
     print("GIF saved to:", output_path)
+
+def gif_selector(video_path, output_gif, start_second=None, start_frame=None, end_frame=None):
+    tmp_frame_path = 'tmp_frame.jpg'
+    cap = cv2.VideoCapture(video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    new_size = (width // 2, height // 2)
+    recording = -1
+    skip_second = 0
+    gif = []
+
+    if start_frame is None:
+        start_frame = int(start_second * fps)
+
+    for idx in range(num_frames):
+        ret, frame = cap.read()
+        if not ret:
+            end_frame = idx
+            break
+
+        if idx < start_frame:
+            continue
+
+        frame = cv2.resize(frame, new_size)
+        cv2.imwrite(tmp_frame_path, frame)
+
+        if not end_frame:
+            if recording != -1:
+                if idx % 3 == 0:
+                    gif.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+            if skip_second > 0:
+                skip_second -= 1
+                continue
+
+            decision = input()
+
+            if decision == '':
+                continue
+            elif decision == 'r':
+                gif.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+                recording = idx
+            elif decision == 's':
+                skip_second = 60
+                print(f"Skipping to frame {idx + skip_second}")
+            elif decision == 'f':
+                end_frame = idx
+                break
+        else:
+            recording = start_frame
+            if idx % 3 == 0:
+                gif.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            if idx >= end_frame:
+                break
+    
+    print(f"Starting frame: {recording}")
+    print(f"End frame: {end_frame}")
+    
+    if len(gif) > 0:
+        imageio.mimsave(output_gif, gif, loop=0)
+        optimize(output_gif)
+    
+    if os.path.exists(tmp_frame_path):
+        os.remove(tmp_frame_path)
+
+    cap.release()
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('video_path', type=str)
+    parser.add_argument('output_path', type=str)
+    parser.add_argument('--start_second', type=int, default=None)
+    parser.add_argument('--start_frame', type=int, default=None)
+    parser.add_argument('--end_frame', type=int, default=None)
+    args = parser.parse_args()
+
+    assert args.start_second is not None or args.start_frame is not None, f"Must use start_second or start_frame. Now they are {args.start_second} and {args.start_frame}"
+    gif_selector(args.video_path, args.output_path, start_second=args.start_second, start_frame=args.start_frame, end_frame=args.end_frame)
+    
