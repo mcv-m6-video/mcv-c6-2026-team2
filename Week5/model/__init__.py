69cb57cca501cb344a25d48b3bc677cf59425f14
loreoregi <loreoregi@gmail.com>
2026-04-07 11:46:56 +0200

    R3d_18 + lstm

--- Week5/model/__init__.py
+++ Week5/model/__init__.py
@@ -2,6 +2,7 @@ from .model_classification import Model as BaselineModel
 from .model_classification_lstm import Model as LSTMModel
 from .model_classification_lstm_attention import Model as LSTMAttnModel
 from .model_classification_3dcnn import Model as Model3DCnn
+from .model_classification_3dcnn_lstm import Model as Model3DCnnLSTM
 
 def get_model(args):
     if args.model_type == "baseline":
@@ -16,6 +17,9 @@ def get_model(args):
     elif args.model_type == "3dcnn":
         print("Using model: 3D CNN")
         return Model3DCnn(args)
+    elif args.model_type == "3dcnn_lstm":
+        print("Using model: 3D NN + LSTM")
+        return Model3DCnnLSTM(args)
     else:
         raise ValueError(f"Unknown model_type: {args.model_type}")
 
