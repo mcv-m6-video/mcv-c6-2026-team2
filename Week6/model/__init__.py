c8126fdfd40284f58731cda3cde32244c1d92def
adriangt2001 <adriangt2001@hotmail.com>
2026-04-11 14:04:42 +0000

    Added LSTM

--- Week6/model/__init__.py
+++ Week6/model/__init__.py
@@ -1,9 +1,13 @@
 from .model_spotting import Model as BaselineModel
+from .model_spotting_lstm import Model as LSTMModel
 
 def get_model(args):
     if args.model_type == "baseline":
         print("Using model: BASELINE")
         return BaselineModel(args)
+    elif args.model_type == "lstm":
+        print("Using model: LSTM")
+        return LSTMModel(args)
     else:
         raise ValueError(f"Unknown model_type: {args.model_type}")
 
