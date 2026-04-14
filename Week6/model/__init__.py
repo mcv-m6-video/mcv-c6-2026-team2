f64c4730afc398ae58cf317072a300fcf7debf6e
loreoregi <loreoregi@gmail.com>
2026-04-14 09:14:18 +0200

    baseline+transformer layers and baseline+multi head attention

--- Week6/model/__init__.py
+++ Week6/model/__init__.py
@@ -1,6 +1,8 @@
 from .model_spotting import Model as BaselineModel
+from .model_spotting_attention import Model as AttentionModel
 from .model_spotting_neck import Model as NeckModel
 from .model_spotting_neck_attention import Model as NeckAttnModel
+from .model_spotting_transformer import Model as TransformerModel
 from .model_spotting_x3d import Model as X3DModel
 from .model_spotting_x3d_neck import Model as X3DNeckModel
 
@@ -8,12 +10,18 @@ def get_model(args):
     if args.model_type == "baseline":
         print("Using model: BASELINE")
         return BaselineModel(args)
+    elif args.model_type == "attention":
+        print("Using model: ATTENTION")
+        return AttentionModel(args)
     elif args.model_type in ["lstm", "gru"]:
         print(f"Using model: {args.model_type.upper()}")
         return NeckModel(args)
     elif args.model_type in ["lstm_attn", "gru_attn"]:
         print(f"Using model: {args.model_type.upper()}")
         return NeckAttnModel(args)
+    elif args.model_type == "transformer":
+        print("Using model: TRANSFORMER")
+        return TransformerModel(args)
     elif args.model_type == "x3d":
         print("Using model: X3D")
         return X3DModel(args)
