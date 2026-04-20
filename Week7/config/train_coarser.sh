8505865dd1c06c8d00f34c62ab654ba97dac3a59
adriangt2001 <adriangt2001@hotmail.com>
2026-04-20 09:08:33 +0000

    Scripts update

--- Week7/config/train_coarser.sh
+++ Week7/config/train_coarser.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 LOG_PATH=data/soccernetball/save_data
-MODELS=(tdeed_rny2_coarse tdeed_rny8_coarse x3d_l_coarse x3d_l_gru_coarse)
+# MODELS=( "tdeed_rny2_coarse" "tdeed_rny8_coarse" "x3d_l_coarse" "x3d_l_gru_coarse" )
+MODELS=( "x3d_l_coarse" "x3d_l_tight" )
 
-for MODEL in $MODELS
+for MODEL in ${MODELS[@]}
 do
     echo Starting $MODEL
-    mkdir $LOG_PATH/$MODELS
+    mkdir -p $LOG_PATH/$MODEL
     CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=2 .venv/bin/python -m main_spotting --model $MODEL | tee $LOG_PATH/$MODEL/log.txt
 done
